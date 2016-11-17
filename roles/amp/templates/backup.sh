fe02851f31330b524958fdb41e52e5f0ca9a819d
Lars Wolter <lars@larswolter.de>
2016-11-17 08:38:03 +0100

    letsencrypt and other stuff added letsencrypt added optional local meteor build fixed php fastcgi fixed backup problem updated node

--- roles/amp/templates/backup.sh
+++ roles/amp/templates/backup.sh
@@ -3,6 +3,6 @@
 # Backup databes encrypted to {{ backup_location }}
 cd /tmp
 echo "dumping mariadb database"
-/usr/bin/mysqldump -u root --password={{ mysql_root_password|default("root") }} | /bin/gzip | /usr/bin/gpg --symmetric --passphrase "{{ backup_password }}" --batch --yes --cipher-algo AES192 --output mariadb-backup-`date +\%Y\%m\%d`.sql.gz.pgp
+/usr/bin/mysqldump -u root --all-databases | /bin/gzip | /usr/bin/gpg --symmetric --passphrase "{{ backup_password }}" --batch --yes --cipher-algo AES192 --output mariadb-backup-`date +\%Y\%m\%d`.sql.gz.pgp
 cp mariadb-backup-`date +\%Y\%m\%d`.sql.gz.pgp {{ backup_location }}/{{ site_name}}
 rm mariadb-backup-`date +\%Y\%m\%d`.sql.gz.pgp
